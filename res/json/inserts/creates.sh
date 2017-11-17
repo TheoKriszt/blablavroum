6b146ddda654e2f3840a7e7ac4adb528c3fbc4f3
tkriszt <theo@kriszt.fr>
2017-11-17 12:15:06 +0100

    rm user.json (unused)

---
+++ res/json/inserts/creates.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+mongoimport --db Covoit --collection vehicules --file vehicules.json --jsonArray
+mongoimport --db Covoit --collection membres --file membres.json --jsonArray
+mongoimport --db Covoit --collection trajets --file trajets.json --jsonArray
+
+# Dans mongo
+# show dbs
+# use Covoit
+# show collections
+# db.membres.find({'prenom':'Marin'})
--- server.js
+++ server.js
@@ -4,36 +4,66 @@ var app=express();
 
 var mongoClient=require("mongodb").MongoClient;
 //creation var db
-var url="mongodb://localhost:27017/blablavroum";
+var url="mongodb://localhost:27017/Covoit";
+
+var sendRes = function(res, json){
+    res.setHeader("Content-type","application/json");
+    res.end(json);
+}
 
 //connection db
 mongoClient.connect(url,function(err,db){
 
-	//requête prenom
-	app.get("/membres/prenom/:prenom",function(req,res){
-		db.collection("membres").find({"prenom":req.params.prenom})
-			.toArray(function(err,documents){
-		//récuperation du résultat
-		var json=JSON.stringify(documents);
-		//renvoie du resultat
-		res.setHeader("Content-type","application/json");
 
-		res.end(json);
-		});
-	}); 
+    //requête tous le smembres
+    app.get("/membres",function(req,res){
+        db.collection("membres").find()
+            .toArray(function(err,documents){
+                //récuperation du résultat
+                var json=JSON.stringify(documents);
+                //renvoi du resultat
+                sendRes(res, json);
 
-	//requête nom
-	app.get("/membres/nom/:nom",function(req,res){
-		db.collection("membres").find({"nom":req.params.prenom})
-			.toArray(function(err,documents){
-		//récuperation du résultat
-		var json=JSON.stringify(documents);
-		//renvoie du resultat
-		res.setHeader("Content-type","application/json");
+            });
+    });
 
-		res.end(json);
-		});
-	}); 
+    //requête prenom
+    app.get("/membres/prenom/:prenom",function(req,res){
+        db.collection("membres").find({"prenom":req.params.prenom})
+            .toArray(function(err,documents){
+                //récuperation du résultat
+                var json=JSON.stringify(documents);
+                //renvoi du resultat
+                sendRes(res, json);
+            });
+    });
+
+    //requête nom
+    app.get("/membres/nom/:nom",function(req,res){
+        db.collection("membres").find({"nom":req.params.prenom})
+            .toArray(function(err,documents){
+                //récuperation du résultat
+                var json=JSON.stringify(documents);
+                //renvoie du resultat
+                res.setHeader("Content-type","application/json");
+
+                res.end(json);
+            });
+    });
+
+    //Ajoute un membre
+    app.put("/membres/add/:nom/:prenom/:mail",function(req,res){
+        db.collection("membres").insertOne({
+            'nom':req.params.nom,
+            'prenom':req.params.prenom,
+            'mail':req.params.mail
+        });
+    });
+
+    //Retire un membre
+    app.delete("/membres/:mail/",function(req,res){
+        db.collection("membres").remove({'mail':req.params.mail});
+    });
 
  
 });
