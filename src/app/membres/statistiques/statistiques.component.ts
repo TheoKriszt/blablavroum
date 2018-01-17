5205302e2db4b5e5052e3e399dfacf4390249d0a
mdodard <mdodard@f6.fdsetu.infra.umontpellier.fr>
2018-01-17 14:25:55 +0100

    haa

--- src/app/membres/statistiques/statistiques.component.ts
+++ src/app/membres/statistiques/statistiques.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MembresService } from '../membres.service';
+import { TrajetsService } from '../../trajets/trajets.service';
 
 @Component({
   selector: 'app-statistiques',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatistiquesComponent implements OnInit {
 
-  constructor() { }
+ public data: any;
 
-  ngOnInit() {
-  }
+ constructor(private mservice: MembresService, private tservice : TrajetsService){}
+    
+ ngOnInit() {
+
+ this.mservice.getTranchesAges().subscribe(mongoRes => {
+  
+   this.data = {
+     labels: ['12-25','26-35','36-50', '50+'],
+     datasets: [
+     {
+	data: [mongoRes.age1825, mongoRes.age2635, mongoRes.age3650, mongoRes.age50],
+	backgroundColor: [
+	   "#84095b",
+	   "#962410",
+	   "#0b6817",
+	   "#270d99"
+	],
+	    hoverBackgroundColor: [
+		"#84095b",
+		"#962410",
+		"#0b6817",
+		"#270d99"
+                    ]
+                }]    
+            };
+      }
+
+
+});
+
+        
 
 }
